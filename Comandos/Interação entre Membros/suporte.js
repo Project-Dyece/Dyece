693ef2d611118b47884a928e5b2d54c96953ae4d
Hanake0 <67035049+Hanake0@users.noreply.github.com>
2020-08-25 17:29:45 -0300

    Add files via upload

---
+++ Comandos/Interação entre Membros/suporte.js
@@ -0,0 +1,52 @@
+const { Command } = require('../../CommandoV12/src/index.js');
+const Discord = require('discord.js');
+
+module.exports = class SuporteCommand extends Command {
+  constructor(client) {
+    super(client, {
+      name: 'suporte',
+      aliases: ['staff'],
+      group: 'interação',
+      memberName: 'suporte',
+      clientPermissions: ['ADMINISTRATOR'],
+      guildOnly: true,
+      description: 'Cria um chat separado para entrar em contato com a Staff',
+      details: 'Serve para entrar em contato com a staff facilmente em caso de dúvida ou sugestão caso, por exemplo, não estejam online.',
+      args: [
+        {
+            key: 'motivo',
+            prompt: 'Com o quê podemos ajudar?',
+            type: 'string',
+            default: '',
+        }
+      ]
+    });
+  }
+  
+  async run(message, { motivo }) {
+    const mencao = `suporte ${message.author.username}  ${message.author.id}`
+    const canal = {
+      type: "text",
+      parent: '728074741792899123',
+      permissionOverwrites: [
+        {
+          id: message.author.id,
+          allow: ['VIEW_CHANNEL', 'SEND_MESSAGES'],
+        },
+        {
+          id: message.guild.id,
+          deny: ['VIEW_CHANNEL'],
+        },
+      ],
+    }
+    
+    const canalSuporte = await message.guild.channels.create(mencao, canal, "canal de denuncia");
+    message.delete()
+    canalSuporte.send(`Canal criado com sucesso ${message.author}.`);
+    if (!motivo) return canalSuporte.send('Com o que podemos ajudar?');
+    const embed = new Discord.MessageEmbed()
+        .setColor('#24960e')
+        .setDescription(`Motivo de contato: ${motivo}`);
+    canalSuporte.send(embed);
+}
+};
---
+++ index.js
@@ -0,0 +1,104 @@
+const commando = require('./CommandoV12/src/index.js');
+const { CommandoClient} = require('./CommandoV12/src/index.js');
+const path = require('path');
+const { readdirSync } = require('fs');
+
+
+//inicializa o lowDB
+const low = require('lowdb');
+const FileSync = require('lowdb/adapters/FileSync');
+
+const adapter = new FileSync('usersOffDB.json');
+const usersOffDB = low(adapter);
+
+//inicializa o banco de dados (firebase) e exporta o banco Online
+const firebase = require('firebase/app');
+const FieldValue = require('firebase-admin').firestore.FieldValue;
+const admin = require('firebase-admin');
+const serviceAccount = require('./ServiceAccount.json');
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://dyece-cfb24.firebaseio.com"
+});
+
+let db = admin.firestore();
+module.exports.db = db;
+
+//guarda os dados localmente e exporta o banco Offline
+let usersOn = db.collection('usuarios').doc('usuarios');
+module.exports.usersOn = usersOn;
+
+usersOn.get().then(snap => {
+	var usersOff = new Map(Object.entries(snap.data()));
+
+    usersOff.forEach(user => {
+        usersOffDB.set(`${user.id}`, user).write();
+    });
+
+    module.exports.usersOffDB = usersOffDB;
+});
+
+setInterval(async () => {
+	usersOn.update(usersOffDB.getState()).then( () => console.log('Update concluído com suscesso'));
+}, 3600000);
+
+
+
+//cria um client do Comando
+const donos = new Set();
+  donos.add('380512056413257729');
+  donos.add('348664615175192577');
+const client = new CommandoClient({
+	commandPrefix: 't//',
+	unknownCommandResponse: false,
+	owner: donos,
+	disableEveryone: true
+});
+
+
+//registra os comandos no client do Commando
+client.registry
+	.registerDefaults()
+	.registerGroups([
+		['autorespostas', 'Auto Respostas'],
+		['utilidades', 'Utilidades'],
+		['interação', 'Interação entre Membros'],
+		['singleplayer', 'Jogos Single Player'],
+		['imgs-ia', 'Imagens Geradas com Inteligência Artificial'],
+		['p&i', 'Perfil & Inventário'],
+		['adm', 'Reservados á Staff'],
+		['eventos', 'Relacionados a Eventos']
+	])
+	.registerCommandsIn(path.join(__dirname, 'Comandos'));
+
+
+//Event Handler(Project-A) && erros
+const evtFiles = readdirSync('./Eventos/');
+console.log('log', `Carregando o total de ${evtFiles.length} eventos`);
+evtFiles.forEach(f => {
+  const eventName = f.split('.')[0];
+  const event = require(`./Eventos/${f}`);
+
+
+  client.on(eventName, event.bind(null, client));
+});
+  client
+	.on('error', console.error)
+	.on('warn', console.warn)
+	.on('debug', (debug) => { if (!debug.includes('[WS => ')) console.log(debug); })
+	.on('disconnect', () => { console.warn('Disconectado!'); })
+	.on('reconnecting', () => { console.warn('Reconectando...'); })
+	.on('commandError', (cmd, err) => {
+		if(err instanceof commando.FriendlyError) return;
+		console.error(`Erro no comando ${cmd.groupID}:${cmd.memberName}`, err);
+	})
+	.on('commandBlocked', (msg, reason) => {
+		console.log(oneLine`
+			Comando ${msg.command ? `${msg.command.groupID}:${msg.command.memberName}` : ''}
+			bloqueado; ${reason}
+		`);
+	});
+
+//login && token
+client.login('NzQ3NjA2Njc4OTU0MDQ5NTc3.X0RU-g.JmP7SxskBVrGLypOmyDU6mVNu0c');
