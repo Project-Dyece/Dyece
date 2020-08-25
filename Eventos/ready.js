652ce21c5489cc4bff600822c563e03bb3deee95
Breno G H <bghaesee@outlook.com>
2020-08-25 18:02:34 -0300

    Arrumada geral

--- Eventos/ready.js
+++ Eventos/ready.js
@@ -2,9 +2,9 @@ module.exports = async (client) => {
 
 
   console.log(`Logado como ${client.user.tag}! (${client.user.id})`);
-	client.guilds.cache.find((a) => a.id === '698560208309452810').channels.cache.find((a) => a.id === '732710544330457161').send(`Online!`);
+	client.guilds.cache.find((a) => a.id === '747643336185348268').channels.cache.find((a) => a.id === '747922393623691356').send(`Online!`);
 	setInterval(async () => {
-	let users = client.guilds.cache.find(a => a.id === '698560208309452810').members.cache.size - 1;
+	let users = client.users.cache.size - 1;
 
     await client.user.setActivity(`${users} usuário${users !== 1 ? 's' : ''}`, {type: 'WATCHING'})
     .catch(err => console.error());
