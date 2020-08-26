adc09cf6fefc89e274ddf8b5017fac63dc8d33ec
Breno G H <bghaesee@outlook.com>
2020-08-25 23:23:03 -0300

    Update: add ESlint

--- index.js
+++ index.js
@@ -1,63 +1,57 @@
 const commando = require('./CommandoV12/src/index.js');
-const { CommandoClient} = require('./CommandoV12/src/index.js');
+const { CommandoClient } = require('./CommandoV12/src/index.js');
 const path = require('path');
 const { readdirSync } = require('fs');
 
-
-//inicializa o lowDB
+// inicializa o lowDB
 const low = require('lowdb');
 const FileSync = require('lowdb/adapters/FileSync');
 
 const adapter = new FileSync('usersOffDB.json');
 const usersOffDB = low(adapter);
 
-//inicializa o banco de dados (firebase) e exporta o banco Online
-const firebase = require('firebase/app');
-const FieldValue = require('firebase-admin').firestore.FieldValue;
+// inicializa o banco de dados (firebase) e exporta o banco Online
 const admin = require('firebase-admin');
 const serviceAccount = require('./ServiceAccount.json');
 
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-  databaseURL: "https://dyece-cfb24.firebaseio.com"
+	credential: admin.credential.cert(serviceAccount),
+	databaseURL: 'https://dyece-cfb24.firebaseio.com',
 });
 
-let db = admin.firestore();
+const db = admin.firestore();
 module.exports.db = db;
 
-//guarda os dados localmente e exporta o banco Offline
-let usersOn = db.collection('usuarios').doc('usuarios');
+// guarda os dados localmente e exporta o banco Offline
+const usersOn = db.collection('usuarios').doc('usuarios');
 module.exports.usersOn = usersOn;
 
-usersOn.get().then(snap => {
-	var usersOff = new Map(Object.entries(snap.data()));
+usersOn.get().then((snap) => {
+	const usersOff = new Map(Object.entries(snap.data()));
 
-    usersOff.forEach(user => {
-        usersOffDB.set(`${user.id}`, user).write();
-    });
+	usersOff.forEach((user) => {
+		usersOffDB.set(`${user.id}`, user).write();
+	});
 
-    module.exports.usersOffDB = usersOffDB;
+	module.exports.usersOffDB = usersOffDB;
 });
 
 setInterval(async () => {
-	usersOn.update(usersOffDB.getState()).then( () => console.log('Update concluído com suscesso'));
+	usersOn.update(usersOffDB.getState()).then(() => console.log('Update concluído com suscesso'));
 }, 3600000);
 
-
-
-//cria um client do Comando
+// cria um client do Comando
 const donos = new Set();
-  donos.add('380512056413257729');
-  donos.add('395592015611428864');
+donos.add('380512056413257729');
+donos.add('395592015611428864');
 const client = new CommandoClient({
 	commandPrefix: 'd?',
 	unknownCommandResponse: false,
 	owner: donos,
-	disableEveryone: true
+	disableEveryone: true,
 });
 
-
-//registra os comandos no client do Commando
+// registra os comandos no client do Commando
 client.registry
 	.registerDefaults()
 	.registerGroups([
@@ -68,37 +62,35 @@ client.registry
 		['imgs-ia', 'Imagens Geradas com Inteligência Artificial'],
 		['p&i', 'Perfil & Inventário'],
 		['adm', 'Reservados á Staff'],
-		['eventos', 'Relacionados a Eventos']
+		['eventos', 'Relacionados a Eventos'],
 	])
 	.registerCommandsIn(path.join(__dirname, 'Comandos'));
 
+module.exports.client = client;
 
-//Event Handler(Project-A) && erros
+// Event Handler(Project-A) && erros
 const evtFiles = readdirSync('./Eventos/');
 console.log('log', `Carregando o total de ${evtFiles.length} eventos`);
-evtFiles.forEach(f => {
-  const eventName = f.split('.')[0];
-  const event = require(`./Eventos/${f}`);
+evtFiles.forEach((f) => {
+	const eventName = f.split('.')[0];
+	const event = require(`./Eventos/${f}`);
 
-
-  client.on(eventName, event.bind(null, client));
+	client.on(eventName, event.bind(null, client));
 });
-  client
+client
 	.on('error', console.error)
 	.on('warn', console.warn)
 	.on('debug', (debug) => { if (!debug.includes('[WS => ')) console.log(debug); })
 	.on('disconnect', () => { console.warn('Disconectado!'); })
 	.on('reconnecting', () => { console.warn('Reconectando...'); })
 	.on('commandError', (cmd, err) => {
-		if(err instanceof commando.FriendlyError) return;
+		if (err instanceof commando.FriendlyError) return;
 		console.error(`Erro no comando ${cmd.groupID}:${cmd.memberName}`, err);
 	})
 	.on('commandBlocked', (msg, reason) => {
-		console.log(oneLine`
-			Comando ${msg.command ? `${msg.command.groupID}:${msg.command.memberName}` : ''}
-			bloqueado; ${reason}
-		`);
+		console.log(`Comando ${msg.command ? `${msg.command.groupID}:${msg.command.memberName}` : ''} bloqueado; ${reason}`);
 	});
 
-//login && token
+
+// login && token
 client.login(process.env.DISCORD_AUTH_TOKEN);
