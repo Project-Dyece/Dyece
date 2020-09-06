f99382280458048c501bea33ab69785ef009b53b
Lucaas <69483583+Lucaas1@users.noreply.github.com>
2020-09-06 00:16:01 -0300

    Update config.js

--- Comandos/Reservados á Staff/config.js
+++ Comandos/Reservados á Staff/config.js
@@ -1,1 +1,42 @@
 
+const { Command } = require('../../CommandoV12/src/index.js');
+const { configsOffDB } = require('../../index.js');
+
+module.exports = class config extends Command {
+  constructor(client) {
+    super(client, {
+      name: 'config',
+      aliases: ['configurar', 'setar'],
+      group: 'adm',
+      memberName: 'config',
+      userPermissions: ['MANAGE_GUILD'],
+      guildOnly: true,
+      description: 'Configure o canal de bem vindo e de suporte.',
+      args: [
+        {
+          key: 'config',
+          prompt: 'Você gostaria de adicionar ou remover a configuraçãos?(add/remover)',
+          type: 'string',
+          oneOf: ['add', 'remover', 'remove', 'rem'],
+        },
+        {
+          key: 'nome',
+          prompt: 'valor para a configuração',
+          type: 'Boolean',
+          default: '',
+				}
+			],
+    });
+  }
+
+  async run(message, { config, nome }) { //alterações no firebase
+    switch (config) {
+        case 'autoreact':
+          configsOffDB.get(message.guild.id).update('autoreact', nome);
+        case 'economia-global':
+          configsOffDB.get(message.guild.id).update('economia-global', nome);
+          
+      }
+  }
+};
+
